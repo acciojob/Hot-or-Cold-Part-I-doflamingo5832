c7ef33d1ea0d23dd499eee33384a96b0236257ac
doflamingo <122207223+doflamingo5832@users.noreply.github.com>
2023-03-04 01:34:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-//your code here
+const randomNumber = Math.floor(Math.random() * 100) + 1; // generates a random number between 1 and 100
+const output = document.getElementById("respond");
+const guessInput = document.getElementById("guess");
 
+function checkGuess() {
+  const userGuess = parseInt(guessInput.value);
+  if (userGuess === randomNumber) {
+    output.innerHTML = "Congratulations! You guessed the correct number!";
+  } else if (userGuess > randomNumber) {
+    output.innerHTML = "Hotter";
+  } else {
+    output.innerHTML = "Colder!";
+  }
+}
